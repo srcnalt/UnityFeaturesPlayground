ce029131ab5ab0465530443949ec5c2db79f1714
Sarge <sercan.altundas@gmail.com>
2018-12-23 22:48:24 +0200

    [feature] created player class
    - health and armor properties are added
    - tests passing

---
+++ Assets/Topics/1 - TDD in Unity/Code/Player.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+namespace TestDrivenDev
+{
+    public class Player : MonoBehaviour
+    {
+        private int _health = 100;
+        private int _armor = 0;
+
+        public int Health {
+            get { return _health + _armor; }
+            private set { }
+        }
+
+        public int Armor { get; private set; }
+    }
+}
+
